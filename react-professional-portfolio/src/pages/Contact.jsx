e66e257c5401b5398eaf394068186ab6f62c62a4
Meow <tabbathapischke@gmail.com>
2024-01-03 10:19:27 -1000

    add basic contact info

---
+++ react-professional-portfolio/src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+export default function Contact() {
+    return (
+      <div>
+        <h1>Contact Me </h1>
+        <h3>Ph: (763) 300-1868 </h3>
+        <h3>Email: tabbathapischke@gmail.com</h3>
+        <h3>LinkedIn </h3>
+        <h3>GitHub </h3>
+      </div>
+    );
+  }
+  
