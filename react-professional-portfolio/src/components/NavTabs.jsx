6a1f75e621150a807cdec20e82ca9b7f3957e83c
Meow <tabbathapischke@gmail.com>
2024-01-03 10:05:28 -1000

    add nav link active class to nav tabs

--- react-professional-portfolio/src/components/NavTabs.jsx
+++ react-professional-portfolio/src/components/NavTabs.jsx
@@ -1,5 +1,57 @@
 import { Link, useLocation } from 'react-router-dom';
 
-
+function NavTabs() {
+    const currentPage = useLocation().pathname;
+  
+    return (
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <Link
+            to="/"
+            // If on home page, we set the url to / and page to class 'nav-link-active', otherwise we set it to 'nav-link'
+            className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+          >
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/About"
+            // If on About page, we set the url to /About and page to class 'nav-link-active', otherwise we set it to 'nav-link'
+            className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
+          >
+            About
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/Portfolio"
+            // If on Portfolio page, we set the url to /Portfolio and page to class 'nav-link-active', otherwise we set it to 'nav-link'
+            className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+          >
+            Portfolio
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/Resume"
+            // If on Resume page, we set the url to /Resume and page to class 'nav-link-active', otherwise we set it to 'nav-link'
+            className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+          >
+            Resume
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/Contact"
+            // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+    );
+  }
 
 export default NavTabs;
