8fbbff0cf8d5d68bcc5bbffa0a89cfc82cdceff1
Meow <tabbathapischke@gmail.com>
2024-01-05 09:03:39 -1000

    formatting, add Header.jsx content

---
+++ react-professional-portfolio/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+
+    return (
+        <div>
+            <h1>Tabbatha Pischke</h1>
+            <p>Web Developer Extraordinaire</p>
+        </div>
+    );
+}
+
+export default Header;
--- react-professional-portfolio/src/pages/About.jsx
+++ react-professional-portfolio/src/pages/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
     return (
         <div>
-            <h1>About Me</h1>
+            <h3>About Me</h3>
             <p>
                 Full Stack Web Developer with a unique background in the healthcare field. Completing a certificate in Full Stack Web Development from the University of California Berkeley Coding Boot Camp. Critical thinker with a passion for solving complex problems. Strengths include teamwork, multitasking, and a strong desire to learn new skills as well as improve existing skills. Pursuing a career path that allows me to utilize my current skills and apply them in new creative ways.
 
