2bdebef51038b397592a85d6e2a63b5c27fbe723
Meow <tabbathapischke@gmail.com>
2024-01-09 14:48:57 -1000

    add bio img, adjust styling

--- react-professional-portfolio/src/pages/About.jsx
+++ react-professional-portfolio/src/pages/About.jsx
@@ -1,11 +1,16 @@
+import mePhoto from '../assets/images/me-2024.jpg';
+
 export default function About() {
     return (
-        <div>
+        <div class="container px-4">
+        <div className="row gx-5">
             <h3>About Me</h3>
-            <p>
+            <img src={mePhoto} className="rounded col-3 p-4" alt="Photo of Tabbatha"></img>
+            <p className="col-6 p-4">
                 Full Stack Web Developer with a unique background in the healthcare field. Completing a certificate in Full Stack Web Development from the University of California Berkeley Coding Boot Camp. Critical thinker with a passion for solving complex problems. Strengths include teamwork, multitasking, and a strong desire to learn new skills as well as improve existing skills. Pursuing a career path that allows me to utilize my current skills and apply them in new creative ways.
 
             </p>
+        </div>
         </div>
     );
 }
