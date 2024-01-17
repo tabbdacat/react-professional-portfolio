45cd5dd5babe67fb9bb669709e11c20b48e4a8a1
Meow <tabbathapischke@gmail.com>
2024-01-16 16:13:30 -1000

    make Footer inline, add margins

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,22 +2,22 @@ import { Link, useLocation } from 'react-router-dom';
 
 function Footer() {
     return (
-        <footer className="text-center float-none">
+        <footer className="text-center float-none m-4">
         <Link
           to="https://github.com/tabbdacat"
-          className= 'nav-link nav-item'
+          className= 'nav-link nav-item  d-inline px-3'
         >
           GitHub
         </Link>
           <Link
             to="https://www.linkedin.com/in/tabbatha-pischke-9a0675288/"
-            className= 'nav-link nav-item'
+            className= 'nav-link nav-item  d-inline px-3'
           >
             LinkedIn
           </Link>
           <Link
             to="https://stackoverflow.com/users/23202330/tabbdacat"
-            className= 'nav-link nav-item'
+            className= 'nav-link nav-item d-inline px-3'
           >
             Stack Overflow
           </Link>
