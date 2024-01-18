e5d567d684222374f07220242ed50a0408fd618c
Meow <tabbathapischke@gmail.com>
2024-01-18 10:51:27 -1000

    add downloadable pdf to resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,7 +1,27 @@
 export default function Resume() {
+
+    const onResumeClick = () => {
+        const pdfUrl = "resume-2024.pdf";
+        const link = document.createElement("a");
+        link.href = pdfUrl;
+        link.download = "resume-2024.pdf"; // specify the filename
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
         <div>
-            <h2>Resume</h2>
+            <h3>Resume and Proficiencies</h3>
+            <div className="text-center">
+            <p >Download Resume PDF here:</p>
+            <button 
+            className="btn btn-outline-light mb-4"
+            onClick={onResumeClick}
+            >
+                Download PDF
+            </button>
+            </div>
             <h4>Proficiencies</h4>
             <ul>
                 <li>
