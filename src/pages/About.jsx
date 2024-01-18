020e80077ddff116cbd51baa4fe54f78a048be4c
Meow <tabbathapischke@gmail.com>
2024-01-18 11:47:19 -1000

    adjust display on img and bio on About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,15 +2,19 @@ import mePhoto from '../assets/images/me-2024.jpg';
 
 export default function About() {
     return (
-        <div className="container px-4">
-        <div className="row gx-5">
-            <h3>About Me</h3>
-            <img src={mePhoto} className="rounded col-3 p-4" alt="Photo of Tabbatha"></img>
-            <p className="col-6 p-4">
-                Full Stack Web Developer with a unique background in the healthcare field. Completing a certificate in Full Stack Web Development from the University of California Berkeley Coding Boot Camp. Critical thinker with a passion for solving complex problems. Strengths include teamwork, multitasking, and a strong desire to learn new skills as well as improve existing skills. Pursuing a career path that allows me to utilize my current skills and apply them in new creative ways.
+        <div>
+                <h3>About Me</h3>
+                <div className="container row  text-center">
+                <div className="col">
+                    <img src={mePhoto} className="pic-me img-fluid rounded col p-2" alt="Photo of Tabbatha"></img>
+                </div>
+                <div className="col-lg-7 col-md-5 my-auto ">
+                    <p>
+                        Full Stack Web Developer with a unique background in the healthcare field. Completing a certificate in Full Stack Web Development from the University of California Berkeley Coding Boot Camp. Critical thinker with a passion for solving complex problems. Strengths include teamwork, multitasking, and a strong desire to learn new skills as well as improve existing skills. Pursuing a career path that allows me to utilize my current skills and apply them in new creative ways.
 
-            </p>
-        </div>
+                    </p>
+                </div>
+                </div>
         </div>
     );
 }
